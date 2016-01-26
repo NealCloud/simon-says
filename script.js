6a3cd8af028f306acf83fbe58d588d48af11dce5
Neal Cloud <neal.cloud@gmail.com>
2016-01-26 13:50:40 -0800

    add timeout NC

--- script.js
+++ script.js
@@ -10,36 +10,45 @@ data = {
         green: 0,
         yellow: 0,
         red: 0,
+        lives: 3,
+        humanTurn: false,
         //start pattern number
         startInt: 2,
+        countInt: 2,
         flashInt: null,
         //computer moves array
         cpuArray: [],
         //player moves array
         playerColorArray: [],
         //simon color values
-        colorArray: ["red","blue","green","yellow"],
+        colorArray: ["red","green","blue","yellow"],
         //jquery simon button array
         btnArray: []
 }
 
-model = {
-    getColor: function(color){
-        return data[color];
-    },
-    setColor: function(color){
-        return data[color];
-    },
+controller = {
+    getColor: function(color){return data[color];  },
+    setColor: function(color){ return data[color]; },
+
     getbtnArray: function(){return data.btnArray;},
     getplayerColorArray: function() {return data.playerColorArray;},
     getcpuArray: function() {return data.cpuArray;},
+    shiftcputArray: function() {return data.cpuArray.shift()},
     getcolorArray: function() {return data.colorArray;},
     getstartInt: function() {return data.startInt;},
+    setstartInt: function(num) {data.startInt = num;},
+    getCountInt: function() {return data.countInt;},
+    setCountInt: function(num) {data.countInt = num;},
+    getlives: function() {return data.lives;},
+    setlives: function(num) {data.lives = num;},
     clearMoveArrays: function(){data.cpuArray = [], data.playerColorArray = [];},
     getFlashInt: function(){return data.flashInt},
-    setFlashInt: function(interval){data.flashInt = interval;}
-}
+    setFlashInt: function(){data.flashInt = setTimeout(view.flashColor(), 2000)},
+    clearFlashInt: function(){clearTimeout(data.flashInt);},
 
+    getHumanTurn: function(){return data.humanTurn},
+    setHumanTurn: function(val){data.humanTurn = val}
+}
 
 view = {
     //initialize board
@@ -56,7 +65,7 @@ view = {
             type: "text",
             text: "start"
         }).addClass("start");
-        //create simon divs
+        //create simon color divs
         view.simonBtn("red");
         view.simonBtn("green");
         view.simonBtn("blue");
@@ -64,66 +73,110 @@ view = {
 
         $('body').append(start);
         $('body').append(text);
+        //create start button link
         $(".start").click(function(){
-            view.pattern(6);
+            view.pattern(controller.getstartInt());
         });
     },
-    // create buttons
+    // create color buttons
     simonBtn: function(color){
         var btn = $("<div>").addClass("hitbox " + color);
         $('.simon').append(btn);
         //push buttons into array for later use;
-        var btnArr = model.getbtnArray();
+        var btnArr = controller.getbtnArray();
         btnArr.push($(this));
+        //add button;
         $(btn).click(function(){
             console.log(color + " clicked");
-            console.log(model.getColor(color));
-            model.getplayerColorArray().push(color);
-            $(this).addClass("gold").delay(300).queue(function(next){
-                $(this).removeClass("gold");
-                next();
-            })
+            console.log(controller.getColor(color));
+            controller.getplayerColorArray().push(color);
+
+            if(controller.getHumanTurn()){
+
+                $(this).addClass("gold").delay(200).queue(function(next){
+                    $(this).removeClass("gold");
+                    next();
+                });
+
+                var newp = controller.shiftcputArray();
+                var count = controller.getcpuArray().length;
+
+                if(newp == color && count == 0){
+                    console.log("you win");
+                    controller.setHumanTurn(false);
+                    controller.setstartInt(controller.getstartInt() + 1);
+                    controller.setCountInt(controller.getstartInt());
+                    view.pattern(controller.getstartInt());
+                }
+
+                else if(newp == color) {
+                    console.log("you got it!");
+                }
+
+                else{
+                    console.log("FAIL");
+                    controller.setlives(controller.getlives() - 1);
+                    controller.setHumanTurn(false);
+                    controller.setstartInt(controller.getstartInt() - 1);
+                    controller.setCountInt(controller.getstartInt());
+                    view.pattern(controller.getstartInt());
+                }
+            }
         });
     },
 
     pattern: function(num){
-        console.log("this is player arr: ", model.getplayerColorArray());
-        model.clearMoveArrays();
-        var cpu = model.getcpuArray();
-        var rando, lightup;
+
+        if(controller.getlives()== 0) return console.log('out of lives');
+
+        console.log("this is player arr: ", controller.getplayerColorArray());
+        controller.clearMoveArrays();
+        var cpu = controller.getcpuArray();
+        var rando;
         var lighting = num;
         for(var i = 0; i < num; i++) {
-            rando = Math.floor(Math.random() * 4) + 1;
-            lightup = cpu[rando];
-            cpu.push(model.getcolorArray()[rando]);
+            rando = Math.floor(Math.random() * 3);
+            //console.log(controller.getcolorArray()[rando]);
+            cpu.push(controller.getcolorArray()[rando]);
         }
 
-        console.log("this is cpu: ", model.getcpuArray());
-        console.log("this is player arr: ", model.getplayerColorArray());
+        console.log("this is cpu: ", controller.getcpuArray());
+        console.log("this is player arr: ", controller.getplayerColorArray());
 
         //flashColor pattern sequence
-        model.setFlashInt(setInterval(view.flashColor(10), 1000));
-
+        view.flashColor();
     },
+
     endFlashColor: function() {
         console.log("color flash ended");
-        var flash = model.getFlashInt();
-        flash.clearInterval();
+        controller.clearFlashInt();
     },
 
-    flashColor: function(num) {
+    flashColor: function() {
+        var num = controller.getCountInt();
         --num;
-        if(num <= 0){
-            view.endFlashColor();
-        }
-        console.log("This is the num: " + num);
 
-        $(this).addClass("gold").delay(300).queue(function(next){
-            $(this).removeClass("gold");
+        controller.setCountInt(num);
+        var cpu = controller.getcpuArray();
+
+        console.log("countdown num: " + num);
+        console.log("This is the color: " , cpu[num - 1]);
+
+        var flash = $("." + cpu[num]);
+
+        $(flash).addClass("gold").delay(300).queue(function(next){
+            $(flash).removeClass("gold");
             next();
-        });
-    }
+        })
 
+        if(num >= 0){
+            setTimeout(view.flashColor, 1000);
+        }
+        else{
+            console.log("done");
+            controller.setHumanTurn(true);
+        }
+    }
 }
 
 
